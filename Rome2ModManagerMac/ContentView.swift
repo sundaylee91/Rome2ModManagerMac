d303738991eae8746d8593bf1a09fa1311ae052d
sundaylee91 <836329330@qq.com>
2026-06-22 12:29:18 +0800

    Initial Commit

---
+++ Rome2ModManagerMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rome2ModManagerMac
+//
+//  Created by 李拜天 on 2026/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rome2ModManagerMac/Rome2ModManagerMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rome2ModManagerMacApp.swift
+//  Rome2ModManagerMac
+//
+//  Created by 李拜天 on 2026/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Rome2ModManagerMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
